e670035405092e61f5999a6f73462a40f19ec16a
tiyarspd <tiyar.spd@gmail.com>
2022-04-17 02:48:43 +0800

    aaa

---
+++ coba/src/main/java/com/toas/coba/tiyar.java
@@ -0,0 +1,17 @@
+package com.toas.coba;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+
+public class tiyar { 
+public static void tes(Context _context, String _s) {
+				Toast.makeText(_context, _s, Toast.LENGTH_SHORT).show();
+		}
+	
+
+	
+} 
